be3b9f873a4046887b258832c47f98aa921055fb
Dave Moten <davidmoten@gmail.com>
2012-06-28 07:40:51 +1000

    signalling development

---
+++ xuml-compiler2/src/main/java/xuml/tools/jaxb/compiler/actor/Signal.java
@@ -0,0 +1,22 @@
+package xuml.tools.jaxb.compiler.actor;
+
+import xuml.tools.jaxb.compiler.message.EntityEvent;
+
+public class Signal<T, R> {
+	private final EntityEvent<T, R> entityEvent;
+	private final boolean toSelf;
+
+	public Signal(EntityEvent<T, R> entityEvent, boolean toSelf) {
+		super();
+		this.entityEvent = entityEvent;
+		this.toSelf = toSelf;
+	}
+
+	public EntityEvent<T, R> getEntityEvent() {
+		return entityEvent;
+	}
+
+	public boolean toSelf() {
+		return toSelf;
+	}
+}
