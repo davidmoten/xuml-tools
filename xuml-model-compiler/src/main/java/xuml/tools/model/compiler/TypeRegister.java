677297f5a66c111afaad743008f8d809bf7c03a3
Dave Moten <davidmoten@gmail.com>
2012-07-02 18:52:42 +1000

    fixed test for child class import in TypeRegister

--- xuml-model-compiler/src/main/java/xuml/tools/model/compiler/TypeRegister.java
+++ xuml-model-compiler/src/main/java/xuml/tools/model/compiler/TypeRegister.java
@@ -60,8 +60,7 @@ public class TypeRegister {
 					.startsWith(relativeToClass)
 					&& t.length() > relativeToClass.length()
 					&& t.indexOf('.', relativeToClass.length() + 1) != -1;
-			if (!t.startsWith(relativeToClass)
-					|| t.indexOf(".", relativeToClass.length() + 1) != -1)
+			if (isImmediateChildOfRelativeClass)
 				s.append("import " + t + ";\n");
 		}
 		return s.toString();
