048920dd47d42f079739d2998d12bd22f0b7f344
Dave Moten <davidmoten@gmail.com>
2012-07-03 22:07:00 +1000

    added AssociationsTest

---
+++ xuml-model-compiler-test/src/test/java/xuml/tools/jaxb/compiler/test/AssociationsTest.java
@@ -0,0 +1,31 @@
+package xuml.tools.jaxb.compiler.test;
+
+import org.junit.Test;
+
+public class AssociationsTest {
+
+	@Test
+	public void testOneToOne() {
+		// TODO
+	}
+
+	@Test
+	public void testZeroOneToOne() {
+		// TODO
+	}
+
+	@Test
+	public void testOneToMany() {
+		// TODO
+	}
+
+	@Test
+	public void testZeroOneToMany() {
+		// TODO
+	}
+
+	@Test
+	public void testManyToMany() {
+		// TODO
+	}
+}
