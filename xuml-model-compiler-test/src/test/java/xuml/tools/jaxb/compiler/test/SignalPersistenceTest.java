61e96de79e6f09aae7f325d8417694f32b3401d4
Dave Moten <davidmoten@gmail.com>
2012-07-08 08:14:18 +1000

    working on signal persistence testing

---
+++ xuml-model-compiler-test/src/test/java/xuml/tools/jaxb/compiler/test/SignalPersistenceTest.java
@@ -0,0 +1,72 @@
+package xuml.tools.jaxb.compiler.test;
+
+import static org.junit.Assert.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import one_to_zero_one.A;
+import one_to_zero_one.A.AId;
+import one_to_zero_one.Context;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import xuml.tools.model.compiler.runtime.Signaller;
+import xuml.tools.util.database.DerbyUtil;
+
+public class SignalPersistenceTest {
+
+	private static EntityManagerFactory emf;
+
+	@BeforeClass
+	public static void setup() {
+		DerbyUtil.disableDerbyLog();
+		emf = Persistence.createEntityManagerFactory("one-to-zero-one");
+		Context.setEntityManagerFactory(emf);
+	}
+
+	@AfterClass
+	public static void shutdown() {
+		emf.close();
+	}
+
+	@Test
+	public void testSignalPersistence() throws ClassNotFoundException {
+
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		A.create(new A.AId("hello2", "there2")).persist(em);
+		em.getTransaction().commit();
+
+		// now test can find using id
+		em.getTransaction().begin();
+		assertNotNull(em.find(A.class, new A.AId("hello2", "there2")));
+		em.getTransaction().commit();
+
+		// now test can find using id and Class.forName
+		em.getTransaction().begin();
+		assertNotNull(em.find(Class.forName(A.class.getName()), new A.AId(
+				"hello2", "there2")));
+		em.getTransaction().commit();
+
+		em.close();
+
+		// now test can find using new em, Class.forName and reconstituted id
+		em = emf.createEntityManager();
+		em.getTransaction().begin();
+		AId id = new A.AId("hello2", "there2");
+		Object id2 = Signaller.toObject(Signaller.toBytes(id));
+		assertNotNull(em.find(Class.forName(A.class.getName()), id2));
+		em.getTransaction().commit();
+
+		// Context.persistSignal(a3.getId(), (Class<Entity<A>>) a3.getClass(),
+		// new A.Events.SomethingDone("12c"));
+		// Context.sendSignalsInQueue();
+
+		em.close();
+	}
+
+}
