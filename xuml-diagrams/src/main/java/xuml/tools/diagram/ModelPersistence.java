37d418317147b99c492a624dc47b803c2c4fb46e
Dave Moten <davidmoten@gmail.com>
2012-07-12 15:44:38 +1000

    added TODOs about DatastoreText

--- xuml-diagrams/src/main/java/xuml/tools/diagram/ModelPersistence.java
+++ xuml-diagrams/src/main/java/xuml/tools/diagram/ModelPersistence.java
@@ -3,24 +3,27 @@ package xuml.tools.diagram;
 import xuml.tools.gae.DatastoreText;
 
 public class ModelPersistence {
-	
+
 	private static final String KEY_KIND = "diagram";
 	private static ModelPersistence instance = new ModelPersistence();
-	
+
 	public static ModelPersistence instance() {
 		return instance;
 	}
-	public String getXml(String id){
+
+	public String getXml(String id) {
 		String entity = id + "-model";
 		String property = "model";
+		// TODO change to DatastoreText.instance()
 		DatastoreText ds = new DatastoreText();
 		String result = ds.get(KEY_KIND, entity, property);
 		return result;
 	}
-	
-	public void save(String id, String xml){
-		String entity = id+"-model";
+
+	public void save(String id, String xml) {
+		String entity = id + "-model";
 		String property = "model";
+		// TODO change to DatastoreText.instance()
 		DatastoreText ds = new DatastoreText();
 		ds.put(KEY_KIND, entity, property, xml);
 	}
