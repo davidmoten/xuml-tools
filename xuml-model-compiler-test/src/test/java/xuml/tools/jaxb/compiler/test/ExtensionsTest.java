519791dfea03e23f18af0ae17a1aa6c12676aec1
Dave Moten <davidmoten@gmail.com>
2012-07-16 09:24:59 +1000

    tested multi field generated findBy methods

--- xuml-model-compiler-test/src/test/java/xuml/tools/jaxb/compiler/test/ExtensionsTest.java
+++ xuml-model-compiler-test/src/test/java/xuml/tools/jaxb/compiler/test/ExtensionsTest.java
@@ -1,5 +1,6 @@
 package xuml.tools.jaxb.compiler.test;
 
+import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -36,16 +37,20 @@ public class ExtensionsTest {
 		try {
 			em.getTransaction().begin();
 			A a = new A();
-
+			a.setEmail("fred@somewhere.com");
+			a.setPostcode(2601);
 			a.persist(em);
 
 			// check that field 'one' has been generated
 			assertNotNull(a.getId());
 
 			em.getTransaction().commit();
+
 		} finally {
 			em.close();
 		}
+		assertEquals(1, A.findByEmailPostcode("fred@somewhere.com", 2601)
+				.size());
 	}
 
 	@Test
