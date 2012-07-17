28aa8f71f97d079e843649576fb68e857d6ebde0
Dave Moten <davidmoten@gmail.com>
2012-07-17 14:32:36 +1000

    adding more to many to many test

--- xuml-model-compiler-test/src/test/java/xuml/tools/jaxb/compiler/test/BinaryAssociationManyToManyTest.java
+++ xuml-model-compiler-test/src/test/java/xuml/tools/jaxb/compiler/test/BinaryAssociationManyToManyTest.java
@@ -1,9 +1,12 @@
 package xuml.tools.jaxb.compiler.test;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import one_to_many.Context;
+import many_to_many.A;
+import many_to_many.B;
+import many_to_many.Context;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -27,8 +30,25 @@ public class BinaryAssociationManyToManyTest {
 	}
 
 	@Test
-	public void test() {
-		// TODO
-	}
+	public void testCanCreateManyToMany() {
 
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		A a1 = A.create("thing").persist(em);
+		A a2 = A.create("thing2").persist(em);
+		B b1 = B.create("boo").persist(em);
+		B b2 = B.create("boo2").persist(em);
+		a1.getB().add(b1);
+		a1.getB().add(b2);
+		b1.getA().add(a1);
+		b2.getA().add(a1);
+
+		a2.getB().add(b1);
+		a2.getB().add(b2);
+		b1.getA().add(a2);
+		b2.getA().add(a2);
+
+		em.getTransaction().commit();
+		em.close();
+	}
 }
