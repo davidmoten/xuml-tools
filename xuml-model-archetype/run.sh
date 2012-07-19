7d7633be96460a6d24ca7947ac561bd2aefc05ce
Dave Moten <davidmoten@gmail.com>
2012-07-19 15:35:38 +1000

    added xuml-model-archetype

---
+++ xuml-model-archetype/run.sh
@@ -0,0 +1,7 @@
+mvn archetype:generate                                  \
+  -DarchetypeGroupId=org.github.davidmoten                \
+  -DarchetypeArtifactId=xuml-model-archetype            \
+  -DarchetypeVersion=0.0.1-SNAPSHOT		                \
+  -DgroupId=temp                                \
+  -DartifactId=model-temp                                  \
+
