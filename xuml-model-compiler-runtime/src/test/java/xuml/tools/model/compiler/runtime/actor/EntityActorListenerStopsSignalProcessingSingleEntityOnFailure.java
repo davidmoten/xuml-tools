ccafafb7553ad690c80af231b80961c953590d56
Dave Moten <davidmoten@gmail.com>
2012-07-27 15:47:49 +1000

    added demo for EntityActorListener

---
+++ xuml-model-compiler-runtime/src/test/java/xuml/tools/model/compiler/runtime/actor/EntityActorListenerStopsSignalProcessingSingleEntityOnFailure.java
@@ -0,0 +1,45 @@
+package xuml.tools.model.compiler.runtime.actor;
+
+import java.util.Set;
+
+import xuml.tools.model.compiler.runtime.message.Signal;
+
+import com.google.common.collect.Sets;
+
+public class EntityActorListenerStopsSignalProcessingSingleEntityOnFailure
+		implements EntityActorListener {
+
+	private static Set<String> stoppedEntities = Sets.newHashSet();
+
+	@Override
+	public synchronized void beforeProcessing(Signal<?> signal,
+			EntityActor actor) {
+		if (stoppedEntities.contains(signal.getEntity().uniqueId()))
+			throw new SignalProcessingStoppedException(signal.getEntity()
+					.uniqueId());
+	}
+
+	@Override
+	public void afterProcessing(Signal<?> signal, EntityActor actor) {
+		// do nothing
+	}
+
+	@Override
+	public synchronized void failure(Signal<?> signal, Exception e,
+			EntityActor actor) {
+		if (!(e instanceof SignalProcessingStoppedException))
+			stoppedEntities.add(signal.getEntity().uniqueId());
+	}
+
+	public static class SignalProcessingStoppedException extends
+			RuntimeException {
+
+		private static final long serialVersionUID = 4537196557689877257L;
+
+		public SignalProcessingStoppedException(String message) {
+			super(message);
+		}
+
+	}
+
+}
