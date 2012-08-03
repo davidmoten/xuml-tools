0e68f8ab97f76caa60f17d43b91b985926537bd2
Dave Moten <davidmoten@gmail.com>
2012-08-03 15:21:06 +1000

    write implementation Factory

--- xuml-model-compiler/src/main/java/xuml/tools/model/compiler/BehaviourImplementationWriter.java
+++ xuml-model-compiler/src/main/java/xuml/tools/model/compiler/BehaviourImplementationWriter.java
@@ -35,12 +35,22 @@ public class BehaviourImplementationWriter {
 	}
 
 	private void generate(PrintStream out) {
-
-		out.format("package %s;\n\n", Util.getPackage(behaviourFullClassName));
+		String simpleClassName = Util
+				.getSimpleClassName(behaviourFullClassName);
+		String packageName = Util.getPackage(behaviourFullClassName);
+		out.format("package %s;\n\n", packageName);
 		out.format("<IMPORTS>\n\n");
 		out.format("public class %s implements %s {\n\n",
 				Util.getSimpleClassName(behaviourFullClassName),
 				types.addType(info.getClassFullName() + ".Behaviour"));
+
+		out.format("    private final %s self;\n\n",
+				info.addType(info.getClassFullName()));
+
+		out.format("    public %s(%s entity) {\n", simpleClassName,
+				types.addType(info.getClassFullName()));
+		out.format("        this.self = entity;\n");
+		out.format("    }\n\n");
 
 		// write state names that have signatures
 		Map<String, MyEvent> stateEvent = Maps.newLinkedHashMap();
@@ -55,6 +65,7 @@ public class BehaviourImplementationWriter {
 		}
 
 		for (MyEvent event : stateEvent.values()) {
+			out.format("    @%s\n", types.addType(Override.class));
 			out.format("    public void onEntry%s(%s event) {\n", Util
 					.upperFirst(Util.toJavaIdentifier(event.getStateName())),
 					types.addType(info.getClassFullName() + ".Events."
@@ -67,6 +78,7 @@ public class BehaviourImplementationWriter {
 			for (MyTransition transition : info.getTransitions()) {
 				// constraint is no event overloading
 				if (transition.getEventName().equals(event.getName())) {
+					out.format("    @%s\n", types.addType(Override.class));
 					out.format("    public void onEntry%s(%s event) {\n", Util
 							.upperFirst(Util.toJavaIdentifier(transition
 									.getToState())), types.addType(info
@@ -79,6 +91,22 @@ public class BehaviourImplementationWriter {
 			}
 		}
 
+		out.format("    public static class Factory implements %s {\n",
+				types.addType(info.getClassFullName() + ".BehaviourFactory"));
+		out.format("        @%s\n", types.addType(Override.class));
+		out.format("        public %s create(%s entity) {\n\n",
+				types.addType(info.getClassFullName() + ".Behaviour"),
+				types.addType(info.getClassFullName()));
+		out.format("            return new %s(entity);\n",
+				types.addType(behaviourFullClassName));
+		out.format("        }\n");
+		out.format("    }\n\n");
+
+		out.format("    private static Factory factory = new Factory();\n\n");
+
+		out.format("    public static Factory factory() {\n");
+		out.format("        return factory;\n");
+		out.format("    }\n");
 		out.format("}");
 	}
 }
