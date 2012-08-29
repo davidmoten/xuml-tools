c140a57b31f51352731f3db553526d483ecb17fc
Dave Moten <davidmoten@gmail.com>
2012-08-29 16:52:40 +1000

    added javadoc

--- xuml-model-compiler/src/main/java/xuml/tools/model/compiler/BehaviourImplementationWriter.java
+++ xuml-model-compiler/src/main/java/xuml/tools/model/compiler/BehaviourImplementationWriter.java
@@ -12,18 +12,37 @@ import xuml.tools.model.compiler.ClassInfoBase.MyTransition;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
+/**
+ * Generates entity behaviour class (java source code) containing on-entry
+ * procedures.
+ * 
+ * @author dave
+ * 
+ */
 public class BehaviourImplementationWriter {
 
 	private final ClassInfo info;
 	private final String behaviourFullClassName;
 	private final TypeRegister types = new TypeRegister();
 
+	/**
+	 * Constructor.
+	 * 
+	 * @param info
+	 * @param behaviourFullClassName
+	 */
 	public BehaviourImplementationWriter(ClassInfo info,
 			String behaviourFullClassName) {
 		this.info = info;
 		this.behaviourFullClassName = behaviourFullClassName;
 	}
 
+	/**
+	 * Returns generated java source code for the implementation of the class
+	 * name specified in the constructor of this class.
+	 * 
+	 * @return
+	 */
 	public String generate() {
 		ByteArrayOutputStream bytes = new ByteArrayOutputStream();
 		PrintStream out = new PrintStream(bytes);
@@ -34,6 +53,11 @@ public class BehaviourImplementationWriter {
 		return result;
 	}
 
+	/**
+	 * Writes generated java source code to the given {@link PrintStream}.
+	 * 
+	 * @param out
+	 */
 	private void generate(PrintStream out) {
 		String simpleClassName = Util
 				.getSimpleClassName(behaviourFullClassName);
@@ -44,6 +68,22 @@ public class BehaviourImplementationWriter {
 				Util.getSimpleClassName(behaviourFullClassName),
 				types.addType(info.getClassFullName() + ".Behaviour"));
 
+		writeFields(out, simpleClassName);
+
+		writeOnEntryMethods(out);
+
+		writeFactory(out);
+
+		out.format("}");
+	}
+
+	/**
+	 * Writes required fields to the PrintStream.
+	 * 
+	 * @param out
+	 * @param simpleClassName
+	 */
+	private void writeFields(PrintStream out, String simpleClassName) {
 		out.format("    private final %s self;\n\n",
 				info.addType(info.getClassFullName()));
 
@@ -51,8 +91,14 @@ public class BehaviourImplementationWriter {
 				types.addType(info.getClassFullName()));
 		out.format("        this.self = entity;\n");
 		out.format("    }\n\n");
+	}
 
-		// write state names that have signatures
+	/**
+	 * Writes the onEntry procedure methods.
+	 * 
+	 * @param out
+	 */
+	private void writeOnEntryMethods(PrintStream out) {
 		Map<String, MyEvent> stateEvent = Maps.newLinkedHashMap();
 		for (MyEvent event : info.getEvents()) {
 			if (event.getStateName() != null)
@@ -65,32 +111,43 @@ public class BehaviourImplementationWriter {
 		}
 
 		for (MyEvent event : stateEvent.values()) {
-			out.format("    @%s\n", types.addType(Override.class));
-			out.format("    public void onEntry%s(%s event) {\n", Util
-					.upperFirst(Util.toJavaIdentifier(event.getStateName())),
-					types.addType(info.getClassFullName() + ".Events."
-							+ event.getStateSignatureInterfaceSimpleName()));
-			out.format("        //TODO write implementation here\n");
-			out.format("    };\n\n");
+			writeStateEventOnEntryMethod(out, event);
 		}
 
 		for (MyEvent event : nonStateEvents) {
-			for (MyTransition transition : info.getTransitions()) {
-				// constraint is no event overloading
-				if (transition.getEventName().equals(event.getName())) {
-					out.format("    @%s\n", types.addType(Override.class));
-					out.format("    public void onEntry%s(%s event) {\n", Util
-							.upperFirst(Util.toJavaIdentifier(transition
-									.getToState())), types.addType(info
-							.getClassFullName()
-							+ ".Events."
-							+ event.getSimpleClassName()));
-					out.format("        //TODO write implementation here\n");
-					out.format("    };\n\n");
-				}
+			writeNonStateEventEntryMethods(out, event);
+		}
+	}
+
+	private void writeNonStateEventEntryMethods(PrintStream out, MyEvent event) {
+		for (MyTransition transition : info.getTransitions()) {
+			// constraint is no event overloading
+			if (transition.getEventName().equals(event.getName())) {
+				out.format("    @%s\n", types.addType(Override.class));
+				out.format("    public void onEntry%s(%s event) {\n", Util
+						.upperFirst(Util.toJavaIdentifier(transition
+								.getToState())), types.addType(info
+						.getClassFullName()
+						+ ".Events."
+						+ event.getSimpleClassName()));
+				out.format("        //TODO write implementation here\n");
+				out.format("    };\n\n");
 			}
 		}
+	}
 
+	private void writeStateEventOnEntryMethod(PrintStream out, MyEvent event) {
+		out.format("    @%s\n", types.addType(Override.class));
+		out.format(
+				"    public void onEntry%s(%s event) {\n",
+				Util.upperFirst(Util.toJavaIdentifier(event.getStateName())),
+				types.addType(info.getClassFullName() + ".Events."
+						+ event.getStateSignatureInterfaceSimpleName()));
+		out.format("        //TODO write implementation here\n");
+		out.format("    };\n\n");
+	}
+
+	private void writeFactory(PrintStream out) {
 		out.format("    public static class Factory implements %s {\n",
 				types.addType(info.getClassFullName() + ".BehaviourFactory"));
 		out.format("        @%s\n", types.addType(Override.class));
@@ -107,6 +164,6 @@ public class BehaviourImplementationWriter {
 		out.format("    public static Factory factory() {\n");
 		out.format("        return factory;\n");
 		out.format("    }\n");
-		out.format("}");
 	}
+
 }
