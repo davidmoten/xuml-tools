9a323e947f4eb9f5d5ee59839c3e18875209c706
Dave Moten <davidmoten@gmail.com>
2014-11-17 18:45:46 +1100

    fix archetype generated java

--- xuml-model-archetype/src/main/resources/archetype-resources/src/main/java/App.java
+++ xuml-model-archetype/src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,5 +1,40 @@
 package ${package};
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import test.Context;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
 public class App {
+
+	public static void startup() {
+		// create the entity manager factory
+		EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("testPersistenceUnit");
+
+		// pass the EntityManagerFactory to the generated xuml Context
+		Context.setEntityManagerFactory(emf);
+		
+		// setup behaviour factories and assign them to Context here
+		
+		// set the behaviour factory for the class A
+		// A.setBehaviourFactory(createBehaviourFactory());
+		
+		// send any signals not processed from last shutdown
+		Context.sendSignalsInQueue();
+	}
+	
+	public static void shutdown() {
+		// shutdown the actor system
+		Context.stop();
+
+		// close the entity manager factory if desired
+		Context.close();
+
+	}
 	
 }
--- xuml-model-archetype/src/main/resources/archetype-resources/src/test/java/AppTest.java
+++ xuml-model-archetype/src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -1,27 +1,14 @@
 package ${package};
 
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
 import xuml.tools.util.database.DerbyUtil;
 import test.A;
 import test.B;
 import test.Context;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import xuml.tools.util.database.DerbyUtil;
 
 public class AppTest {
 	
@@ -29,35 +16,17 @@ public class AppTest {
 	@BeforeClass
 	public static void setup() {
 		DerbyUtil.disableDerbyLog();
-
-		// create the entity manager factory
-		EntityManagerFactory emf = Persistence
-				.createEntityManagerFactory("testPersistenceUnit");
-
-		// pass the EntityManagerFactory to the generated xuml Context
-		Context.setEntityManagerFactory(emf);
-		
-		// set the behaviour factory for the class A
-		// A.setBehaviourFactory(createBehaviourFactory());
-		
-		// send any signals not processed from last shutdown
-		Context.sendSignalsInQueue();
-
+		App.startup();
 	}
 	
 	@Test
-	public void dummy() {
-		
+	public void test1() {
+		// your test goes here
 	}
 
 	@AfterClass
-	public static void cleanup() {
-
-		// shutdown the actor system
-		Context.stop();
-
-		// close the entity manager factory if desired
-		Context.close();
+	public static void shutdown() {
+		App.shutdown();
 	}
 
 	
