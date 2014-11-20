840305de10fb0fabed62ae5377c8690c468a1507
Dave Moten <davidmoten@gmail.com>
2014-11-21 06:18:21 +1100

    add enumerate type

--- order-tracker-webapp/src/main/java/ordertracker/rs/Service.java
+++ order-tracker-webapp/src/main/java/ordertracker/rs/Service.java
@@ -1,13 +1,17 @@
 package ordertracker.rs;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import ordertracker.Context;
+import ordertracker.Depot;
 import ordertracker.Order;
 
 @Path("/")
@@ -28,7 +32,7 @@ public class Service {
 				senderEmail, maxAttempts, comment));
 		return Response.status(200).entity("order created").build();
 	}
-	
+
 	@PUT
 	@Path("/order/{orderId}/send")
 	public Response sendOrder(@PathParam("orderId") String orderId) {
@@ -36,39 +40,44 @@ public class Service {
 		order.signal(new Order.Events.Send());
 		return Response.status(200).entity("order sent").build();
 	}
-	
+
 	@PUT
 	@Path("/order/{orderId}/assign")
 	public Response assignToCourier(@PathParam("orderId") String orderId) {
 		Order order = Order.find(orderId);
 		order.signal(new Order.Events.Assign());
-		return Response.status(200).entity("order assigned to a courier").build();
+		return Response.status(200).entity("order assigned to a courier")
+				.build();
 	}
-	
+
 	@PUT
 	@Path("/order/{orderId}/pickedUp")
 	public Response pickedUpByCourier(@PathParam("orderId") String orderId) {
 		Order order = Order.find(orderId);
 		order.signal(new Order.Events.PickedUp());
-		return Response.status(200).entity("order picked up by a courier").build();
+		return Response.status(200).entity("order picked up by a courier")
+				.build();
 	}
-	
+
 	@PUT
 	@Path("/order/{orderId}/arrivedDepot")
-	public Response arrivedDepot(@PathParam("orderId") String orderId, String depotId) {
+	public Response arrivedDepot(@PathParam("orderId") String orderId,
+			String depotId) {
 		Order order = Order.find(orderId);
 		order.signal(new Order.Events.ArrivedDepot(depotId));
-		return Response.status(200).entity("arrived depot "+ depotId).build();
+		return Response.status(200).entity("arrived depot " + depotId).build();
 	}
-	
+
 	@PUT
 	@Path("/order/{orderId}/arrivedFinalDepot")
-	public Response arrivedFinalDepot(@PathParam("orderId") String orderId, @QueryParam("depotId") String depotId) {
+	public Response arrivedFinalDepot(@PathParam("orderId") String orderId,
+			@QueryParam("depotId") String depotId) {
 		Order order = Order.find(orderId);
 		order.signal(new Order.Events.ArrivedFinalDepot(depotId));
-		return Response.status(200).entity("arrived final depot "+ depotId).build();
+		return Response.status(200).entity("arrived final depot " + depotId)
+				.build();
 	}
-	
+
 	@PUT
 	@Path("/order/{orderId}/delivering")
 	public Response delivering(@PathParam("orderId") String orderId) {
@@ -76,7 +85,7 @@ public class Service {
 		order.signal(new Order.Events.Delivering());
 		return Response.status(200).entity("delivering").build();
 	}
-	
+
 	@PUT
 	@Path("/order/{orderId}/delivered")
 	public Response delivered(@PathParam("orderId") String orderId) {
@@ -84,7 +93,7 @@ public class Service {
 		order.signal(new Order.Events.Delivered());
 		return Response.status(200).entity("delivered").build();
 	}
-	
+
 	@PUT
 	@Path("/order/{orderId}/deliveryFailed")
 	public Response deliveryFailed(@PathParam("orderId") String orderId) {
@@ -92,7 +101,7 @@ public class Service {
 		order.signal(new Order.Events.DeliveryFailed());
 		return Response.status(200).entity("delivery failed").build();
 	}
-	
+
 	@PUT
 	@Path("/order/{orderId}/deliveryAgain")
 	public Response deliverAgain(@PathParam("orderId") String orderId) {
@@ -100,5 +109,13 @@ public class Service {
 		order.signal(new Order.Events.DeliverAgain());
 		return Response.status(200).entity("marked for delivery again").build();
 	}
-	
+
+	@GET
+	@Path("/depot/{depotId}/ordersReadyForDelivery")
+	@Produces("application/json")
+	public Response getOrdersReadyForDelivery(
+			@PathParam("depotId") String depotId) {
+		Depot depot = Depot.find(depotId);
+		return Response.ok("{}", MediaType.APPLICATION_JSON).build();
+	}
 }
