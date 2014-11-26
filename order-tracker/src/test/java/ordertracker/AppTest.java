135601fae11e2949e3b5140335160e8071029e7a
Dave Moten <davidmoten@gmail.com>
2014-11-26 21:32:25 +1100

    fix unit test so uses latches and observable subscription to wait for completion

--- order-tracker/src/test/java/ordertracker/AppTest.java
+++ order-tracker/src/test/java/ordertracker/AppTest.java
@@ -2,10 +2,11 @@ package ordertracker;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-
-import javax.persistence.EntityManager;
 
 import ordertracker.Order.State;
 
@@ -13,6 +14,7 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import rx.Observer;
 import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 import xuml.tools.util.database.DerbyUtil;
@@ -44,21 +46,40 @@ public class AppTest {
 
 	@Test(timeout = 10000)
 	public void testDeliverySequence() throws InterruptedException {
+		final List<String> states = new ArrayList<>();
+		final CountDownLatch latch = new CountDownLatch(1);
+		EventService.instance().events().take(5).subscribeOn(Schedulers.io())
+				.subscribe(new Observer<String>() {
+
+					@Override
+					public void onCompleted() {
+						latch.countDown();
+					}
+
+					@Override
+					public void onError(Throwable e) {
+
+					}
+
+					@Override
+					public void onNext(String state) {
+						states.add(state);
+					}
+				});
+
 		Order order = Context.create(Order.class, new Order.Events.Create("2",
 				"test order", "canberra", "sydney", "fred@yahoo.com",
 				"joey@gmail.com", 3, "created"));
+
 		Context.create(Depot.class, new Depot.Events.Create("2", "Bungendore",
 				-35.0, 142.0));
-		checkState(Order.State.PREPARING);
 		order.signal(new Order.Events.Send());
-		checkState(Order.State.READY_FOR_DISPATCH);
 		order.signal(new Order.Events.Assign());
-		checkState(Order.State.COURIER_ASSIGNED);
 		order.signal(new Order.Events.PickedUp());
-		checkState(Order.State.IN_TRANSIT);
 		order.signal(new Order.Events.ArrivedDepot("2"));
-		checkState(Order.State.IN_TRANSIT);
-		TimeUnit.MILLISECONDS.sleep(500);
+		latch.await();
+		assertEquals(Arrays.asList("PREPARING", "READY_FOR_DISPATCH",
+				"COURIER_ASSIGNED", "IN_TRANSIT", "IN_TRANSIT"), states);
 	}
 
 	private void checkState(State state) throws InterruptedException {
