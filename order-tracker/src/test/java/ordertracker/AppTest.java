3419205cd9797b508f695200c470bebbba973141
Dave Moten <davidmoten@gmail.com>
2014-11-28 07:31:30 +1100

    add test but comment it out till working

--- order-tracker/src/test/java/ordertracker/AppTest.java
+++ order-tracker/src/test/java/ordertracker/AppTest.java
@@ -57,23 +57,8 @@ public class AppTest {
 				Order.State.DELIVERED.toString());
 		final CountDownLatch latch = new CountDownLatch(1);
 		EventService.instance().events().take(expectedStates.size())
-				.subscribeOn(Schedulers.io()).subscribe(new Observer<String>() {
-
-					@Override
-					public void onCompleted() {
-						latch.countDown();
-					}
-
-					@Override
-					public void onError(Throwable e) {
-						throw new RuntimeException(e);
-					}
-
-					@Override
-					public void onNext(String state) {
-						states.add(state);
-					}
-				});
+				.subscribeOn(Schedulers.io())
+				.subscribe(createObserver(states, latch));
 
 		Order order = Order.create(new Order.Events.Create("2", "test order",
 				"canberra", "sydney", "fred@yahoo.com", "joey@gmail.com", 3,
@@ -89,6 +74,75 @@ public class AppTest {
 		order.signal(new Order.Events.Delivered());
 		latch.await();
 		assertEquals(expectedStates, states);
+	}
+
+	// keep timeout quite large so that freebie CI servers don't fail when they
+	// are under load
+	// @Test(timeout = 6000)
+	public void testDeliverySequenceWithRetries() throws InterruptedException {
+		final List<String> states = new ArrayList<>();
+		List<String> expectedStates = Arrays.asList(
+				Order.State.PREPARING.toString(),
+				Order.State.READY_FOR_DISPATCH.toString(),
+				Order.State.COURIER_ASSIGNED.toString(),
+				Order.State.IN_TRANSIT.toString(),
+				Order.State.IN_TRANSIT.toString(),
+				Order.State.READY_FOR_DELIVERY.toString(),
+				Order.State.DELIVERING.toString(),
+				Order.State.DELIVERY_FAILED.toString(),
+				Order.State.AWAITING_NEXT_DELIVERY_ATTEMPT.toString(),
+				Order.State.DELIVERING.toString(),
+				Order.State.DELIVERY_FAILED.toString(),
+				Order.State.AWAITING_NEXT_DELIVERY_ATTEMPT.toString(),
+				Order.State.DELIVERING.toString(),
+				Order.State.DELIVERY_FAILED.toString(),
+				Order.State.HELD_FOR_PICKUP.toString(),
+				Order.State.DELIVERED.toString());
+		final CountDownLatch latch = new CountDownLatch(1);
+		EventService.instance().events().take(expectedStates.size())
+				.subscribeOn(Schedulers.io())
+				.subscribe(createObserver(states, latch));
+
+		Order order = Order.create(new Order.Events.Create("3", "test order",
+				"canberra", "sydney", "fred@yahoo.com", "joey@gmail.com", 3,
+				"created"));
+
+		Depot.create(new Depot.Events.Create("3", "Bungendore", -35.0, 142.0));
+		order.signal(new Order.Events.Send());
+		order.signal(new Order.Events.Assign());
+		order.signal(new Order.Events.PickedUp());
+		order.signal(new Order.Events.ArrivedDepot("2"));
+		order.signal(new Order.Events.ArrivedFinalDepot("2"));
+		order.signal(new Order.Events.Delivering());
+		order.signal(new Order.Events.DeliveryFailed());
+		order.signal(new Order.Events.DeliverAgain());
+		order.signal(new Order.Events.DeliveryFailed());
+		order.signal(new Order.Events.DeliverAgain());
+		order.signal(new Order.Events.DeliveryFailed());
+		order.signal(new Order.Events.DeliveredByPickup());
+		latch.await();
+		assertEquals(expectedStates, states);
+	}
+
+	private Observer<String> createObserver(final List<String> states,
+			final CountDownLatch latch) {
+		return new Observer<String>() {
+
+			@Override
+			public void onCompleted() {
+				latch.countDown();
+			}
+
+			@Override
+			public void onError(Throwable e) {
+				throw new RuntimeException(e);
+			}
+
+			@Override
+			public void onNext(String state) {
+				states.add(state);
+			}
+		};
 	}
 
 	@AfterClass
