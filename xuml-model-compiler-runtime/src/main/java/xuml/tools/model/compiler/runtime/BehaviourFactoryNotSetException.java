6819100afcede7ea7d70d951ad862b4760e5774b
Dave Moten <davidmoten@gmail.com>
2015-08-21 17:33:22 +1000

    throw BehaviourNotSetException from generated entity constructors with useful message

---
+++ xuml-model-compiler-runtime/src/main/java/xuml/tools/model/compiler/runtime/BehaviourFactoryNotSetException.java
@@ -0,0 +1,10 @@
+package xuml.tools.model.compiler.runtime;
+
+public class BehaviourFactoryNotSetException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public BehaviourFactoryNotSetException(String message) {
+        super(message);
+    }
+}
