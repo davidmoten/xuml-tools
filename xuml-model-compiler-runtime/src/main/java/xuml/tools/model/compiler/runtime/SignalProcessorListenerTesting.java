1a6114d32e1fb610621c3eb8ffec03a26a2fcba4
Dave Moten <davidmoten@gmail.com>
2015-08-26 18:31:07 +1000

    use slf4j logging

--- xuml-model-compiler-runtime/src/main/java/xuml/tools/model/compiler/runtime/SignalProcessorListenerTesting.java
+++ xuml-model-compiler-runtime/src/main/java/xuml/tools/model/compiler/runtime/SignalProcessorListenerTesting.java
@@ -2,8 +2,9 @@ package xuml.tools.model.compiler.runtime;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import xuml.tools.model.compiler.runtime.actor.EntityActor;
 import xuml.tools.model.compiler.runtime.message.Signal;
@@ -12,21 +13,23 @@ public class SignalProcessorListenerTesting implements SignalProcessorListener {
 
     private final List<Exception> exceptions = new CopyOnWriteArrayList<>();
 
-    private static Logger log = Logger.getLogger(SignalProcessorListenerTesting.class.getName());
+    private static Logger log = LoggerFactory.getLogger(SignalProcessorListenerTesting.class);
 
     @Override
     public void beforeProcessing(Signal<?> signal, EntityActor actor) {
-        log.fine("before processing " + signal);
+        log.debug("before processing " + signal);
     }
 
     @Override
     public void afterProcessing(Signal<?> signal, EntityActor actor) {
-        log.fine("after processing " + signal);
+        log.debug("after processing " + signal);
     }
 
     @Override
     public void failure(Signal<?> signal, Exception e, EntityActor actor) {
-        log.log(Level.SEVERE, e.getMessage(), e);
+        // log the failure
+        String msg = "error processing " + signal + ": " + e.getMessage();
+        log.error(msg, e);
         exceptions.add(e);
     }
 
