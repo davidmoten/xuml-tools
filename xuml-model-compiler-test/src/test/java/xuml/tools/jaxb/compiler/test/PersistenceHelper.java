a62539448a813a51d1cc98bcb3f33be9ac1511a3
Dave Moten <davidmoten@gmail.com>
2015-10-04 19:22:52 +1100

    reduce logging verbosity in xuml-model-compiler-test and set MAVEN_OPTS -Xmx512m in .travis.yml

--- xuml-model-compiler-test/src/test/java/xuml/tools/jaxb/compiler/test/PersistenceHelper.java
+++ xuml-model-compiler-test/src/test/java/xuml/tools/jaxb/compiler/test/PersistenceHelper.java
@@ -34,7 +34,7 @@ public class PersistenceHelper {
         Map<String, String> map = Maps.newHashMap();
         map.put("hibernate.hbm2ddl.auto", "create-drop");
         map.put("hibernate.connection.pool_size", "5");
-        map.put("hibernate.show_sql", "true");
+        map.put("hibernate.show_sql", "false");
         map.put("hibernate.format_sql", "true");
         if (type == DatabaseType.DERBY) {
             String dialect = "org.hibernate.dialect.DerbyDialect";
